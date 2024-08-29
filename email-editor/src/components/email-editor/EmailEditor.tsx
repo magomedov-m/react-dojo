b7784a4295de7961f35d2e36aa44116dfed55c2a
magomedov-m <magomedov.dgu@gmail.com>
2024-08-29 17:01:22 +0300

    Завершен проект по работе с текстом

--- email-editor/src/components/email-editor/EmailEditor.tsx
+++ email-editor/src/components/email-editor/EmailEditor.tsx
@@ -1,25 +1,67 @@
+import React, { useState, useRef } from "react";
 import { Eraser, Bold, Italic, Underline } from "lucide-react";
 import style from "../email-editor/EmailEditor.module.sass";
+import { applyStyle } from "./apply-style";
+import parse from "html-react-parser";
 
 export default function EmailEditor() {
+  const [selectionStart, setSelectionStart] = useState(0);
+  const [selectionEnd, setSelectionEnd] = useState(0);
+  const [text, setText] = useState(
+    "Hey hello - this is email editor. My project - good luck"
+  );
+  const textRef = useRef<HTMLAreaElement | null>(null);
+  const updateSelection = () => {
+    if (!textRef.current) return;
+    setSelectionStart(textRef.current.selectionStart);
+    setSelectionEnd(textRef.current.selectionEnd);
+  };
+  const applyFormat = (type: "bold" | "italic" | "underline") => {
+    const before = text.substring(0, selectionStart);
+    const after = text.substring(selectionEnd);
+    const selectedText = text.substring(selectionStart, selectionEnd);
+
+    setText(before + applyStyle(type, selectedText) + after);
+    console.log(selectedText);
+  };
+
   return (
     <div>
       <h1>Email editor</h1>
-
+      <div className={style.preview}>{parse(text)}</div>
       <div className={style.card}>
-        <div className={style.editor}>hey lorem ipsum</div>
+        <textarea
+          contentEditable
+          className={style.editor}
+          spellCheck={false}
+          onSelect={updateSelection}
+          ref={textRef}
+          value={text}
+          readOnly
+          onChange={(e) => setText(e.target.value)}
+        >
+          hey lorem ipsum hey lorem ipsum hey lorem ipsum hey lorem ipsum hey
+          lorem ipsum hey lorem ipsum hey lorem ipsum hey lorem ipsum hey lorem
+          ipsum hey lorem ipsum hey lorem ipsumem ipsum
+        </textarea>
         <div className={style.actions}>
           <div className={style.tools}>
-            <button className={style.icon}>
+            <button className={style.icon} onClick={() => setText("")}>
               <Eraser />
             </button>
-            <button className={style.icon}>
+            <button className={style.icon} onClick={() => applyFormat("bold")}>
               <Bold />
             </button>
-            <button className={style.icon}>
+            <button
+              className={style.icon}
+              onClick={() => applyFormat("italic")}
+            >
               <Italic />
             </button>
-            <button className={style.icon}>
+            <button
+              className={style.icon}
+              onClick={() => applyFormat("underline")}
+            >
               <Underline />
             </button>
           </div>
--- email-editor/src/components/email-list/EmailList.tsx
+++ email-editor/src/components/email-list/EmailList.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import style from '../email-list/EmailList.module.sass'
 
 export default function EmailList() {
   return (
-    <div>EmailList</div>
+    <div className={style.list}>
+      <div>
+        handom text hey lorem ipsum hey lorem ipsum hey lorem ipsum hey lorem ipsum hey lorem ipsum hey lorem ipsum hey lorem ipsum hey lorem ipsum
+      </div>
+    </div>
   )
 }
